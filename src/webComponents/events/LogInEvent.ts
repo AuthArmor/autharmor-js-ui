81527cb2f089fe43295b4fb21cbaaf07c9ca0852
saraelsa <sara-etx@outlook.com>
2023-06-10 12:05:02 -0400

    Fix stray import

--- src/webComponents/events/LogInEvent.ts
+++ src/webComponents/events/LogInEvent.ts
@@ -1,4 +1,4 @@
-import { IAuthenticationSuccessResult } from "../../../client";
+import { IAuthenticationSuccessResult } from "autharmor-sdk";
 
 export class LogInEvent extends Event {
     constructor(public readonly authenticationResult: IAuthenticationSuccessResult) {

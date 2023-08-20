8ceec3300a30a96daa7d987793d470739a8d509a
Sara Elsayeda <swe@saraelsa.net>
2023-08-20 08:26:24 -0400

    Fix local environment committed

--- samples/autharmor-angular-sample/src/config/environment.ts
+++ samples/autharmor-angular-sample/src/config/environment.ts
@@ -1,9 +1,6 @@
 export const environment = {
     frontendBaseUrl: `${window.location.protocol}//${window.location.host}`,
-    // backendBaseUrl: "[Backend base URL]",
-    // clientSdkApiKey: "[Client SDK API key]",
-    // webAuthnClientId: "[WebAuthn client ID]"
-    backendBaseUrl: "http://localhost:3001",
-    clientSdkApiKey: "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjhiNzhkYTFmLWU4NWEtNDg4Ny05MmQxLTcyNWZlNTg3MGFiZCJ9.eyJrZXkiOiJYVGdYc2hpaWdOTWRwNXZLTjJoQ2ZVZDdCVHlzdUJEZUgzWkpOM0xCckVweXV2YXoiLCJ2IjoiNCIsImlhdCI6MTY5MjM3MzQ3OX0.ieVKZi3zx2FxjBIWDFIzx--Bj-ZGtR6vRwFf5AHBrKf2wysQBF98WZ1Aj6xjGH-KGbLI5lbpeRsgHTTttAFxDA",
-    webAuthnClientId: "cdc34e48-1805-487d-9572-1adcc013f80b"
+    backendBaseUrl: "[Backend base URL]",
+    clientSdkApiKey: "[Client SDK API key]",
+    webAuthnClientId: "[WebAuthn client ID]"
 };

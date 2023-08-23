fa5f2463d57def9bfa5383ff6d314634e3006d72
Sara Elsayeda <swe@saraelsa.net>
2023-08-23 17:39:32 -0400

    Fix isMobile set to true for development

--- src/common/isMobile.ts
+++ src/common/isMobile.ts
@@ -1,6 +1,4 @@
-// export const isMobile =
-//     "userAgentData" in navigator
-//         ? (navigator["userAgentData"] as { mobile: boolean }).mobile
-//         : false;
-
-export const isMobile = true;
+export const isMobile =
+    "userAgentData" in navigator
+        ? (navigator["userAgentData"] as { mobile: boolean }).mobile
+        : false;

ccb97dfd16a44cfb7f973f899dafe27c63f07a0e
Sara Elsayeda <swe@saraelsa.net>
2023-08-24 09:44:59 -0400

    Add ability to customize UI options

--- src/options/defaultUiOptions.ts
+++ src/options/defaultUiOptions.ts
@@ -1,3 +1,37 @@
 import { IUiOptions } from "./IUiOptions";
 
-export const defaultUiOptions: IUiOptions = {};
+export const defaultUiOptions: IUiOptions = {
+    appLinkBackgroundColor: "hsl(0deg 0% 95%)",
+    appLinkIconForegroundColor: "hsl(0deg 0% 15%)",
+    appLinkLabelForegroundColor: "hsl(0deg 0% 15%)",
+    qrCodeForegroundColor: "hsl(0deg 0% 15%)",
+    qrCodeErrorForegroundColor: "hsl(0deg 0% 75%)",
+    verificationCodeForegroundColor: "hsl(0deg 0% 30%)",
+    verificationCodeValueForegroundColor: "hsl(290deg 100% 40%)",
+    formMethodTitleForegroundColor: "hsl(0deg 0% 15%)",
+    formMethodDescriptionForegroundColor: "hsl(0deg 0% 30%)",
+    promptForegroundColor: "hsl(0deg 0% 15%)",
+    promptDescriptionForegroundColor: "hsl(0deg 0% 30%)",
+    promptErrorForegroundColor: "hsl(0deg 100% 40%)",
+    formButtonBackgroundColor: "hsl(0deg 0% 15%)",
+    formButtonForegroundColor: "hsl(0deg 0% 100%)",
+    formButtonHoverBackgroundColor: "hsl(0deg 0% 30%)",
+    formButtonHoverForegroundColor: "hsl(0deg 0% 100%)",
+    formButtonDisabledBackgroundColor: "hsl(0deg 0% 45%)",
+    formButtonDisabledForegroundColor: "hsl(0deg 0% 90%)",
+    formFieldLabelForegroundColor: "hsl(0deg 0% 15%)",
+    formFieldErrorForegroundColor: "hsl(0deg 100% 40%)",
+    formInputBackgroundColor: "hsl(0deg 0% 95%)",
+    formInputForegroundColor: "hsl(0deg 0% 15%)",
+    optionCardBackgroundColor: "hsl(0deg 0% 95%)",
+    optionCardHoverBackgroundColor: "hsl(290deg 50% 95%)",
+    optionCardIconForegroundColor: "hsl(0deg 0% 15%)",
+    optionCardLabelForegroundColor: "hsl(0deg 0% 15%)",
+    optionCardDescriptionForegroundColor: "hsl(0deg 0% 30%)",
+    qrCodeLoaderColor: "hsl(0deg 0% 40%)",
+    qrCodeLoaderActiveColor1: "hsl(0deg 0% 40%)",
+    qrCodeLoaderActiveColor2: "hsl(0deg 0% 15%)",
+    tabForegroundColor: "hsl(0deg 0% 45%)",
+    tabHoverForegroundColor: "hsl(0deg 0% 15%)",
+    tabActiveForegroundColor: "hsl(290deg 100% 40%)"
+};

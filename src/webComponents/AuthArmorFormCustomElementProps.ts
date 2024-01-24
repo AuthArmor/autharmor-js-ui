37c0bd98d097f430294b7d950a0d1d52e4f9f110
Sara Elsayeda <swe@saraelsa.net>
2024-01-24 11:16:48 -0500

    Add additional types relating to events

--- src/webComponents/AuthArmorFormCustomElementProps.ts
+++ src/webComponents/AuthArmorFormCustomElementProps.ts
@@ -2,15 +2,22 @@ import { AuthArmorFormProps } from "../form/AuthArmorFormProps";
 
 export type AuthArmorFormCustomElementProps = Omit<
     AuthArmorFormProps,
-    | "client"
+    "client" | AuthArmorFormCustomElementEventName
+> & { client: AuthArmorFormProps["client"] | null };
+
+export type AuthArmorFormCustomElementEventName =
     | "onLogIn"
     | "onRegister"
     | "onOutOfBandLogIn"
     | "onOutOfBandRegister"
     | "onLogInFailure"
     | "onRegisterFailure"
-    | "onError"
-> & { client: AuthArmorFormProps["client"] | null };
+    | "onError";
+
+export type AuthArmorFormCustomElementEvents = Pick<
+    AuthArmorFormProps,
+    AuthArmorFormCustomElementEventName
+>;
 
 declare global {
     interface HTMLElementTagNameMap {
